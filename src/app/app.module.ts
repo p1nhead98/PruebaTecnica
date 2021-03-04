6de853a41351cecb5427df0077e7cfbe16eae316
gyigas998 <vcarrascop999@gmail.com>
2021-03-04 14:03:49 -0300

    Componentes login/data, tokerService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { fromEventPattern } from 'rxjs';
+import { DataComponent } from './components/data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { stringify } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import {Router} from "@angular/router";
+
+export interface Cuenta {
+  mail: string,
+  password: string
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+
+export class LoginComponent implements OnInit {
+  title = 'PruebaBarracks';
+  cuenta: Cuenta = {
+    mail: '',
+    password: ''
+  };
+  constructor(
+    private ServiceBack: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    document.body.style.background = "  url('../assets/images/LoginBackground.jpg') no-repeat center center fixed ";
+   if(localStorage.getItem('Token') ){
+    this.router.navigate(['/account/data']);
+   }
+  }
+  Token(obj: Cuenta) {
+    this.ServiceBack.getToken(obj).subscribe(
+      res => {
+   
+        localStorage.setItem('Token', res.token);
+        const lStorage = localStorage.getItem('Token');
+        if(lStorage){
+          this.router.navigate(['/account/data']);
+        }
+      },
+      error => {
+        console.log("Error xd")
+      }
+    )
+  }
+
+}
